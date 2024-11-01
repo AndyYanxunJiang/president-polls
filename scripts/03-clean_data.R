936acccb0b65372540fe42d21c1e382a78494731
AndyYanxunJiang <ayx.jiang@mail.utoronto.ca>
2024-11-01 18:45:32 -0400

    update result and discussion

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rui Hu, 
+# Purpose: Cleans the raw data into 7 swing states
+# Author: Rui Hu, Andy Jiang
 # Date: 21 October 2024
-# Contact: hr.hu@mail.utoronto.ca, 
+# Contact: hr.hu@mail.utoronto.ca, ayx.jiang@mail.utoronto.ca
 # License: N/A
 # Pre-requisites: N/A
 
@@ -14,9 +14,11 @@ library(tidyverse)
 raw_data <- read_csv("data/01-raw_data/raw_data.csv")
 
 cleaned_data <- raw_data |> 
-  filter(population == "rv", numeric_grade >=3) |>
-  select(pollster,state,start_date,end_date,question_id,sample_size,
-           population,party,answer,candidate_name,hypothetical, pct)
+  filter(population == "rv", 
+         state %in% c("Pennsylvania", "Georgia", "North Carolina", 
+                      "Michigan", "Arizona", "Wisconsin", "Nevada")) |>
+  select(pollster, state, start_date, end_date, question_id, sample_size,
+         population, party, answer, candidate_name, hypothetical, pct)
 
 #### Save data ####
 write_csv(cleaned_data, "data/02-analysis_data/analysis_data.csv")
