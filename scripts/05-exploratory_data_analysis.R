a37101bfd51f165333ef64afadd53990689aa3c4
XYPKQ896 <hr.hu@mail.utoronto.ca>
2024-11-04 16:38:23 -0500

    Update exploratory data.

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -1,11 +1,12 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models voter support for U.S. 2024 presidential candidates in key swing states,
+#          analyzing polling trends and predicting election outcomes.
+# Author: Andy Jiang
+# Date: 30 October 2024
+# Contact: ayx.jiang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: N\A
+# Any other information needed? N\A
 
 
 #### Workspace setup ####
@@ -13,19 +14,19 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
-
+data_filtered <- read_csv(here::here("data/02-analysis_data/model_data.csv"))
 ### Model data ####
-first_model <-
+model <- 
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
+    formula = pct ~ end_date_numeric + candidate_name, 
+    data = data_filtered, 
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+    seed = 304
   )
+summary(model)
 
 
 #### Save model ####
